38bbf1c1225b38531ddcc32ebdaf311d0be2d0d9
AhmadSamih <ahmadsamih@gmail.com>
2016-06-05 00:40:18 -0500

    [217. Contains Duplicate][Accepted]committed by LeetCode Extension

--- 217-Contains-Duplicate/solution.cpp
+++ 217-Contains-Duplicate/solution.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp; int size = nums.size();
-        for(int i=size-1;i>=0;i--){
-            int cnt = ++mp[nums[i]];
-            if(cnt > 1) return true;
-        }
-        return false;
+       set<int> st(nums.begin(),nums.end());
+       return (st.size()<nums.size());
     }
 };
