3ce66864075fee514ba3b60873c4407a239447c8
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-09 02:54:46 -0500

    [39. Combination Sum][Accepted]committed by LeetCode Extension

--- 39-Combination-Sum/solution.cpp
+++ 39-Combination-Sum/solution.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    unordered_map<string, int>seen;
+    vector<int>tmp;
+    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>tmp;
         sort(candidates.begin(), candidates.end());
-        recurr(candidates, tmp, target);
+        recurr(candidates, target, 0);
         return res;
     }
     
-    void recurr(vector<int>& candidates, vector<int>&tmp, int target){
-        int sum = 0; for(auto s:tmp) sum+=s;
+    void recurr(vector<int>& candidates, int target, int idx){
+        int sum = 0; for(int s=0; s<tmp.size();s++) sum+=tmp[s];
         if(sum == target){
-            vector<int> tmp2 = tmp;
-            sort(tmp2.begin(), tmp2.end());
-            string str = "";
-            for(auto x:tmp2) str+=to_string(x);
-            if(seen[str]) return;
-            seen[str] = 1;
-            res.push_back(tmp2);
+            res.push_back(tmp);
             return;
         }else if(sum>target) return;
 
-        for(int i=0;i<candidates.size();i++){
+        for(int i=idx;i<candidates.size();i++){
             tmp.push_back(candidates[i]);
-            recurr(candidates, tmp, target);
+            recurr(candidates, target, i); //to avoid repetition, always start from the current index
             tmp.pop_back();//backtrack
         }
     }
