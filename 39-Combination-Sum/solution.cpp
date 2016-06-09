8ec69964c6bca38708ef503c83d2ccdbf126138b
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-09 03:01:56 -0500

    [39. Combination Sum][Accepted]committed by LeetCode Extension

--- 39-Combination-Sum/solution.cpp
+++ 39-Combination-Sum/solution.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    vector<int>tmp;
+    vector<int>tmp; int target;
+    int size;
     
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int _target) {
         sort(candidates.begin(), candidates.end());
-        recurr(candidates, target, 0, 0);
+        target = _target; size = candidates.size();
+        recurr(candidates, 0, 0);
         return res;
     }
     
-    void recurr(vector<int>& candidates, int target, int idx, int sum){
+    void recurr(vector<int>& candidates, int idx, int sum){
         if(sum == target){
             res.push_back(tmp);
             return;
         }else if(sum>target) return;
 
-        for(int i=idx;i<candidates.size();i++){
+        for(int i=idx;i<size;i++){
             tmp.push_back(candidates[i]);
-            recurr(candidates, target, i, sum+candidates[i]); //to avoid repetition, always start from the current index
+            recurr(candidates, i, sum+candidates[i]); //to avoid repetition, always start from the current index
             tmp.pop_back();//backtrack
         }
     }
