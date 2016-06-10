586c4e2a484c25fb69c52d01a8779695353a6817
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-10 00:41:40 -0500

    [108. Convert Sorted Array to Binary Search Tree][Accepted]committed by Ahmad Samih

--- 108-Convert-Sorted-Array-to-Binary-Search-Tree/solution.cpp
+++ 108-Convert-Sorted-Array-to-Binary-Search-Tree/solution.cpp
@@ -12,26 +12,15 @@ public:
     TreeNode *head;
     TreeNode* sortedArrayToBST(vector<int>& nums) {
         if(nums.empty()) return head;
-        int l = 0, r = nums.size()-1;
-        int mid = (l+r+1)/2;
-        head = new TreeNode(nums[mid]);
-        insert(head, l, mid-1, nums);
-        insert(head, mid+1, r, nums);
-        return head;
+        return insert(0, nums.size()-1, nums);
     }
     
-    void insert(TreeNode* parent, int l, int r, vector<int>&nums){
-        if(l>r) return;
-        int mid = (l+r+1)/2; TreeNode *tmp;
-        if(nums[mid] < parent->val) {
-            parent->left = new TreeNode(nums[mid]); tmp = parent->left;
-        }
-        if(nums[mid] >= parent->val) {
-            parent->right = new TreeNode(nums[mid]); tmp = parent->right;
-        }
-        if(l==r)return;
-        insert(tmp, l, mid-1, nums);
-        insert(tmp, mid+1, r, nums);
+    TreeNode* insert(int l, int r, vector<int>&nums){
+        if(l>r) return NULL;
+        int mid = (l+r+1)/2; TreeNode* root = new TreeNode(nums[mid]);
+        root->left = insert(l, mid-1, nums); 
+        root->right = insert(mid+1, r, nums);
+        return root;
     }
     
 };
