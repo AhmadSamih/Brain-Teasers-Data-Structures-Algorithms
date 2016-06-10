fe7ac918f1cf1dc47591719f74e4a9c838d3e3c6
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-09 22:39:03 -0500

    [77. Combinations][Accepted]committed by Ahmad Samih

--- 77-Combinations/solution.cpp
+++ 77-Combinations/solution.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    vector<int> tmp;
     int n, k;
 
     vector<vector<int>> combine(int _n, int _k){
-        tmp.resize(_k,0); n = _n; k = _k;
-        recurr(0, 0);
+        vector<int> tmp(_k,0); n = _n; k = _k;
+        recurr(0, 0,tmp);
         return res;
     }
     
-    void recurr(int idx, int size){
+    void recurr(int idx, int size, vector<int>&tmp){
         if(size == k){
             res.push_back(tmp);
             return;
         }
         for(int i=idx;i<n;i++){
             tmp[size++]=i+1;
-            recurr(i+1, size);
+            recurr(i+1, size,tmp);
             size--;
         }
     }
