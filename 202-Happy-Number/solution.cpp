77378917f20a48483349425eae51c688292b33ae
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-14 05:54:06 -0500

    [202. Happy Number][Accepted]committed by Ahmad Samih

--- 202-Happy-Number/solution.cpp
+++ 202-Happy-Number/solution.cpp
@@ -4,7 +4,7 @@ public:
         
         if(n<=0)return false;
         int num = n;
-        unordered_map<int, int> hash_table;
+        unordered_map<int, int> hash_table;//could use a set as well to detect cycle, or could use a cycle detection approach, like slow/fast pointers.
         
         while(1){
             int sum =  0;
