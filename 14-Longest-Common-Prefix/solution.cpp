38e587540ec654cdd5ccc393159a54f4b10d3ac0
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-17 00:15:59 -0500

    [14. Longest Common Prefix][Accepted]committed by Ahmad Samih

--- 14-Longest-Common-Prefix/solution.cpp
+++ 14-Longest-Common-Prefix/solution.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.empty()) return "";
-        int i=0, not_found = 0;
-        for(i=0; i<strs[0].size(); i++){
-            for(int j=1; j<strs.size(); j++){
+        int i=0, not_found = 0, size = strs[0].size(), num_strs=strs.size();
+        for(i=0; i<size; i++){
+            for(int j=1; j<num_strs; j++){
                 if(strs[0][i] != strs[j][i]){
                     not_found = 1;
                     break;
