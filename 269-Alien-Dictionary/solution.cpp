8442d36d82b213af627503cfe4b0dfe7a64be8ac
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-19 13:56:20 -0500

    [269. Alien Dictionary][Accepted]committed by Ahmad Samih

--- 269-Alien-Dictionary/solution.cpp
+++ 269-Alien-Dictionary/solution.cpp
@@ -2,46 +2,35 @@ class Solution {
 public:
 
     string res;
-    struct vertix{
-        char v;
-        vertix* adj[26];
-        vertix(char _v){
-            for(int i=0; i<26; i++){
-                adj[i] = NULL;
-            }
-            v = _v;
-        }
-    };
-
-    unordered_map<int, vertix*>cache;
+    
+    //define a graph
+    unordered_map<char,unordered_set<char>> G;
     unordered_set<int>cyc;
 
-    bool topologicalSortUtil(vertix* v, unordered_map<vertix*, bool> &visited, stack<char> &Stack)
+    bool topologicalSortUtil(char n, unordered_map<char, bool> &visited, stack<char> &Stack)
     {
-        if(cyc.count(v->v)) return true;
-        
-        if(visited[v]) return false;
-        visited[v] = true; 
-        cyc.insert(v->v);
+        if(cyc.count(n)) return true;
+        if(visited[n]) return false;
+        visited[n] = true; 
+        cyc.insert(n);
      
-        for(int i =0; i<26; i++)
-            if(v->adj[i]){
-             bool cycle = topologicalSortUtil(v->adj[i], visited, Stack);
+        for(auto itr=G[n].begin(); itr!=G[n].end(); itr++){
+             bool cycle = topologicalSortUtil(*itr, visited, Stack);
              if(cycle)return true;
-            }
+        }
     
-        cyc.erase(v->v);
-        Stack.push(v->v);
+        cyc.erase(n);
+        Stack.push(n);
         return false;
     }
 
     bool topologicalSort()
     {
         stack<char> Stack;
-        unordered_map<vertix*, bool>visited;
+        unordered_map<char, bool>visited;
 
-        for(auto itr=cache.begin(); itr!=cache.end(); itr++){
-            bool cycle = topologicalSortUtil(itr->second, visited, Stack);
+        for(auto itr=G.begin(); itr!=G.end(); itr++){
+            bool cycle = topologicalSortUtil(itr->first, visited, Stack);
             if(cycle) return false;
         }
 
@@ -54,23 +43,13 @@ public:
     
     string alienOrder(vector<string>& words) {
         if(words.size()==1)return words.front();
-        vertix* v = NULL;
         for(int i = 0; i<words.size()-1; i++){
-            string w1 = words[i]; string w2 = words[i+1];
-            int found = 0;
+            string w1 = words[i]; string w2 = words[i+1]; int found = 0;
             for(int j=0; j<max(w1.size(), w2.size()); j++){
-                if(j<w1.size()){
-                    if(cache[words[i][j]]==false)
-                        cache[words[i][j]] = new vertix(words[i][j]); 
-                    v = cache[words[i][j]];
-                }
-                if(j<w2.size()){
-                    if(cache[words[i+1][j]]==false)
-                        cache[words[i+1][j]] = new vertix(words[i+1][j]); 
-                }
-
+                if(j<w1.size() && G.count(w1[j])==0)G[w1[j]] = unordered_set<char>();
+                if(j<w2.size() && G.count(w2[j])==0)G[w2[j]] = unordered_set<char>();
                 if(j<w1.size() && j<w2.size() && w1[j] != w2[j] && !found){
-                    v->adj[words[i+1][j]-'a'] = cache[words[i+1][j]];                
+                    G[w1[j]].insert(w2[j]);
                     found =1;
                 }
             }
