feb7768052533f0c26a71c1b64d383625d5b625f
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-21 01:44:56 -0500

    [221. Maximal Square][Accepted]committed by Ahmad Samih

--- 221-Maximal-Square/solution.cpp
+++ 221-Maximal-Square/solution.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
     if(matrix.size() == 0) return 0;
-    int max=0;
     vector<vector<int>>DP(matrix.size(), vector<int>(matrix[0].size(),0));
-    for(int i=0;  i<matrix.size(); i++){
-        for(int j=0; j<matrix[i].size(); j++){
+    int max=0;
+    for(int i=0;  i<matrix.size(); i++) { DP[i][0] = matrix[i][0] - '0'; max = max<DP[i][0] ? 1 : max;}
+    for(int j=0;  j<matrix[0].size(); j++) {DP[0][j] = matrix[0][j] - '0'; max = max<DP[0][j] ? 1 : max;}
+    for(int i=1;  i<matrix.size(); i++){
+        for(int j=1; j<matrix[i].size(); j++){
             DP[i][j] = matrix[i][j] - '0';
-            if(i>0 && j>0 && DP[i][j]){
-                DP[i][j] += min(DP[i-1][j], min(DP[i][j-1], DP[i-1][j-1]));
-            }
+            DP[i][j] += DP[i][j]*(min(DP[i-1][j], min(DP[i][j-1], DP[i-1][j-1])));
             max = max<DP[i][j] ? DP[i][j] : max;
         }
     }
