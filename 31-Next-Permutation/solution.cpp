6689a5fc7d6acca2b92ece291a3fb445882efebb
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-22 16:01:07 -0500

    [31. Next Permutation][Accepted]committed by Ahmad Samih

--- 31-Next-Permutation/solution.cpp
+++ 31-Next-Permutation/solution.cpp
@@ -5,9 +5,9 @@ public:
         for(int i = nums.size()-2; i>=0; i--){
             if(nums[i] < nums[i+1]){
                 found = 1;            
-                set<int>_set(nums.begin()+i, nums.end());
-                auto idx = _set.find(nums[i]);
-                int next = *(++idx);
+                //set<int>_set(nums.begin()+i, nums.end());
+                //auto idx = _set.find(nums[i]);
+                //int next = *(++idx);
                 int value = INT_MAX;
                 for(int k=nums.size()-1; k>i; k--) 
                     if(nums[k] > nums[i]) {
@@ -17,7 +17,7 @@ public:
                         }
                     }
                 int tmp2 = nums[i];
-                nums[i] = next;        
+                nums[i] = nums[pos];        
                 nums[pos] = tmp2;
                 sort(nums.begin()+i+1, nums.end());
                 break;
