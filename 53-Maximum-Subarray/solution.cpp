88b1d520865bd84402576da7432ad9dea019ca4b
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-21 23:13:21 -0500

    [53. Maximum Subarray][Accepted]committed by Ahmad Samih

--- 53-Maximum-Subarray/solution.cpp
+++ 53-Maximum-Subarray/solution.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int size = nums.size();
         int curr_max =nums[0];
         int max_so_far = nums[0];
         
-        for(int i=1; i<nums.size();i++){
-            curr_max = max(curr_max+nums[i], nums[i]);
-            max_so_far = max(max_so_far, curr_max);
+        for(int i=1; i<size;i++){
+            curr_max = std::max(curr_max+nums[i], nums[i]);
+            max_so_far = std::max(max_so_far, curr_max);
         }
         
         return max_so_far;
