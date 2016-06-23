d0396e34fc6e1857ac265c7ffbb1889628d7195d
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-22 23:32:45 -0500

    [10. Regular Expression Matching][Accepted]committed by Ahmad Samih

--- 10-Regular-Expression-Matching/solution.cpp
+++ 10-Regular-Expression-Matching/solution.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
+    bool helper(string s, int si, string p, int pi){
+        
+        if(si == s.size() && pi == p.size()) return true;
+
+        if(p[pi+1] == '*'){
+            return helper(s,si,p,pi+2) || ((s[si]==p[pi] || p[pi]=='.') && si<s.size() && helper(s,si+1,p,pi));
+        }else{
+            if(si<s.size() && (s[si]==p[pi] || p[pi]=='.')) return helper(s, si+1, p, pi+1);
+            else return false;
+        }
+    }
+    
     bool isMatch(string s, string p) {
-      int m = s.size(), n = p.size(); 
-      vector<vector<bool>> _ismatch(m + 1, vector<bool>(n + 1));
-      _ismatch[0][0] = true;
-
-       for(int j = 1; j <=n; j++){
-           if(p[j-1] == '*')
-            _ismatch[0][j] = (j>1 &&_ismatch[0][j-2]);
-       }
-
-       for(int i =1; i<=m; i++){
-          for(int j =1; j<=n; j++){
-            if(p[j-1] == '*')
-                _ismatch[i][j] = (j>1 &&_ismatch[i][j-2]) || (_ismatch[i-1][j]&&(p[j-2]==s[i-1] || p[j-2]=='.'));
-            else{
-                _ismatch[i][j] = (s[i-1] == p[j-1] || p[j-1] == '.') && _ismatch[i-1][j-1];
-            }
-          }
-      }
-      return _ismatch[m][n];
-
+        
+        return helper(s,0,p,0);
+            
     }
 };
