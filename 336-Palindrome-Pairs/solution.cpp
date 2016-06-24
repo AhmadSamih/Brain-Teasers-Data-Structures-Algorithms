5b4a6bbcd9e1cc05f531b9eb052b24852b1cb7a8
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-24 17:48:04 -0500

    [336. Palindrome Pairs][Accepted]committed by Ahmad Samih

--- 336-Palindrome-Pairs/solution.cpp
+++ 336-Palindrome-Pairs/solution.cpp
@@ -1,45 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> palindromePairs(vector<string>& words) {
-
-    vector<vector<int>> res;
-    //populate the words in a hash map
-    unordered_map<string, int>wrd_idx;
-    for(int i=0; i<words.size();i++){
-        wrd_idx[words[i]] = i;
-    }
-
-    //substr, reverse, 
-    vector<int>set(2);
-    for(int i =0; i<words.size();i++){
-        int len = words[i].size();
-        for(int j=0; j<=len; j++){
-            string left = words[i].substr(0,j);
-            string right = words[i].substr(j);
-            string rleft = left;
-            string rright = right;
-            reverse(rleft.begin(), rleft.end());
-            reverse(rright.begin(), rright.end());
-           
-            if(wrd_idx.find(rleft)!= wrd_idx.end() && wrd_idx[rleft]!=i && (right==rright)){
-                set[0] = i;
-                set[1] = wrd_idx[rleft];
-                res.push_back(set);
-            }
-
-            if(j!=0 && wrd_idx.find(rright)!= wrd_idx.end() && wrd_idx[rright]!=i && (left==rleft)){
-                set[0] = wrd_idx[rright];
-                set[1] = i;
-                res.push_back(set);
+        
+        vector<vector<int>> res;
+        
+        unordered_map<string, int>cache;
+        for(int i=0;i<words.size();i++){
+            cache[words[i]]=i;
+        }
+        
+        for(int i=0; i<words.size();i++){
+            for(int j=0; j<=words[i].size();j++){
+                string left =  words[i].substr(0,j);
+                string right = words[i].substr(j);
+                string rleft = left; reverse(rleft.begin(), rleft.end());
+                string rright = right; reverse(rright.begin(), rright.end());
+                if(cache.find(rleft)!=cache.end() && cache[rleft]!=i && right==rright ){
+                    res.push_back({i,cache[rleft]});
+                }
+                if(j && cache.find(rright)!=cache.end() && cache[rright]!=i && left==rleft ){
+                    res.push_back({cache[rright],i});
+                }
             }
         }
-    }
-    return res;
-    }
-
-    bool isPalindrome(string s) {
-        string tmp = s;
-        reverse(tmp.begin(), tmp.end());
-        return (s == tmp);
+        return res;
     }
 };
