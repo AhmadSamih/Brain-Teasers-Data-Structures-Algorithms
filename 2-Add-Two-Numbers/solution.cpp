84b91d6027fb83d54e5384b5404687694427b494
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-25 23:15:17 -0500

    [2. Add Two Numbers][Accepted]committed by Ahmad Samih

---
+++ 2-Add-Two-Numbers/solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        
+        ListNode * tail = new ListNode(-1);
+        ListNode *head  = tail;
+        
+        while(l1 || l2){
+            
+            int res = (l1?l1->val:0) + (l2?l2->val:0) + carry;
+            if (res>9){
+                tail->next = new ListNode(res-10);
+                carry =1;
+            }else{
+                tail->next = new ListNode(res);
+                carry = 0;
+            }
+            tail = tail->next;
+            if(l1)l1=l1->next;
+            if(l2)l2=l2->next;
+        }
+        if(carry) tail->next = new ListNode(1);
+        return head->next;
+    }
+};
