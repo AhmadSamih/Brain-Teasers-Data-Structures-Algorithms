3ccb03cf63d488fd8ecb77e91a5643e86e3811ef
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-26 15:53:17 -0500

    [202. Happy Number][Accepted]committed by Ahmad Samih

--- 202-Happy-Number/solution.cpp
+++ 202-Happy-Number/solution.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     bool isHappy(int n) {
+        unordered_map<int, int>cache;
         
-        if(n<=0)return false;
-        int num = n;
-        unordered_map<int, int> hash_table;//could use a set as well to detect cycle, or could use a cycle detection approach, like slow/fast pointers.
-        
-        while(1){
-            int sum =  0;
-            while(num>0){
-                int d = num % 10;
-                sum  += (d*d);
-                num /= 10;
+        int sum = 0;
+        for(;;){
+            sum = 0;
+            while(n){
+                int div = n%10;
+                n = n/10;
+                sum+= div*div;
             }
+            n = sum;
             if(sum == 1) return true;
-            if(hash_table[sum]){
-                return false;
-            }else{
-                hash_table[sum] = 1;
-            }
-            num = sum;
+            if(cache.find(sum)!=cache.end())return false;
+            cache[sum] = 1;
         }
     }
 };
