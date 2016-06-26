39d953564812be24806f75c1203fd6e6f28106b9
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-25 23:15:57 -0500

    [2. Add Two Numbers][Accepted]committed by Ahmad Samih

--- 2-Add-Two-Numbers/solution.cpp
+++ 2-Add-Two-Numbers/solution.cpp
@@ -10,12 +10,10 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        
         ListNode * tail = new ListNode(-1);
         ListNode *head  = tail;
         
         while(l1 || l2){
-            
             int res = (l1?l1->val:0) + (l2?l2->val:0) + carry;
             if (res>9){
                 tail->next = new ListNode(res-10);
