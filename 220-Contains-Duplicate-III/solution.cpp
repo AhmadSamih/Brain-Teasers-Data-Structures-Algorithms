b24de26bbc120d2b29d0ca2687ea81a393b0b503
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-26 01:19:54 -0500

    [220. Contains Duplicate III][Accepted]committed by Ahmad Samih

--- 220-Contains-Duplicate-III/solution.cpp
+++ 220-Contains-Duplicate-III/solution.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int _t) {
         if(!nums.size() || !k) return false;
-        vector<int>bst;
+        vector<long long>bst;
         for(int i=0; i<nums.size(); i++)
         {
-            const auto it = lower_bound(bst.begin(), bst.end(), nums[i] - t);
-            if(it != bst.end() && abs(*it - nums[i]) <= t) return true;
+            //const auto it = lower_bound(bst.begin(), bst.end(), nums[i] - t);
+            //if(it != bst.end() && abs(*it - nums[i]) <= t) return true;
+            
+            const auto it = lower_bound(bst.begin(), bst.end(), nums[i]);
+            int idx = it - bst.begin();
+            long long num = nums[i];
+            long long t = _t;
+            if( idx>=0 && idx < bst.size() && abs(bst[idx]-  nums[i])<=t ) return true;
+            if( idx>0 && idx <= bst.size() && abs(bst[idx-1]-nums[i])<=t ) return true;
+
             bst.insert(it, nums[i]);
             if(bst.size() > k) bst.erase( find(bst.begin(), bst.end(), nums[i-k]) );
         }
