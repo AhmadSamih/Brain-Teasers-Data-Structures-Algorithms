61050846f8ff80ee64c01d0284a7b3cb44f40d17
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-27 08:35:31 -0500

    [221. Maximal Square][Accepted]committed by Ahmad Samih

--- 221-Maximal-Square/solution.cpp
+++ 221-Maximal-Square/solution.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-    if(matrix.size() == 0) return 0;
-    int rows = matrix.size();
-    int cols = matrix[0].size();
-    vector<vector<int>>DP(rows, vector<int>(cols,0));
-    int _max=0;
-    
-    for(int i=0;  i<rows; i++) { DP[i][0] = matrix[i][0] - '0'; _max = max(DP[i][0],_max);}
-    for(int j=0;  j<cols; j++) {DP[0][j] = matrix[0][j] -'0'; _max = max(DP[0][j],_max);}
-    
-    for(int i=1;  i<rows; i++){
-        for(int j=1; j<cols; j++){
-            DP[i][j] = matrix[i][j] - '0';
-            DP[i][j] += (-DP[i][j])&(min(DP[i-1][j], min(DP[i][j-1], DP[i-1][j-1])));
-            _max = max(DP[i][j],_max);
+        
+        int x = matrix.size();
+        if(x == 0) return 0;
+        int y = matrix[0].size();
+        int max = INT_MIN;
+        vector<vector<int>>DP(x, vector<int>(y,0));
+        
+        for(int i=0; i<x; i++){
+            DP[i][0] = matrix[i][0]-'0';
+            if(max < DP[i][0]) max = DP[i][0];
         }
-    }
-    return _max*_max;
+
+        for(int j=0; j<y; j++){
+            DP[0][j] = matrix[0][j]-'0';
+            if(max < DP[0][j]) max = DP[0][j];
+        }
+        
+        
+        for(int i = 1; i < x; i++){
+            for(int j = 1; j< y; j++){
+                if(matrix[i][j]-'0')
+                    DP[i][j] = 1 + min(DP[i-1][j-1], min(DP[i-1][j],DP[i][j-1]));
+                else
+                    DP[i][j] = 0;
+                if(max < DP[i][j]) max = DP[i][j];
+            }
+        }
+        return max*max;
     }
 };
