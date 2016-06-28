2dd05420a74dc0113881888e27ebf3de3c6b381e
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-06-28 11:37:19 -0500

    [213. House Robber II][Accepted]committed by Ahmad Samih

---
+++ 213-House-Robber-II/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int helper(vector<int>& nums, int start, int end){
+        int size = end-start+1;
+        int DP[size]= {0};
+        DP[0] = nums[start]; 
+        for(int i=1; i<size;i++){
+            DP[i] = max(DP[i-1], i>1 ? nums[start+i]+DP[i-2]:nums[start+i]);
+        }
+        return DP[size-1];
+    }
+
+    int rob(vector<int>& nums) {
+        if(nums.empty())return 0;
+        if(nums.size()==1)return nums[0];
+        return max(helper(nums, 0, nums.size()-2),helper(nums, 1, nums.size()-1));
+    }
+};
