8b96bc13da05a9cbf2144f65e327ed99f71139e3
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-04 00:41:04 -0500

    [18. 4Sum][Accepted]committed by Ahmad Samih

--- 18-4Sum/solution.cpp
+++ 18-4Sum/solution.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>>res;
+    
     vector<vector<int>> fourSum(vector<int>& nums, int _target) {
+	    vector<vector<int>>res;
+		
+		if(nums.empty()) 
+		    return res;
+        
+        int size = nums.size();
         sort(nums.begin(), nums.end());
         
-        for(int k=0; k<nums.size();k++){
+        for(int k=0; k<size;k++){
             int t = _target - nums[k];
             if(k>0 && nums[k]==nums[k-1]) continue;
-            for(int i=k+1; i<nums.size(); i++){
+        
+            for(int i=k+1; i<size; i++){
                 int target = t - nums[i];
                 if(i>k+1 && nums[i]==nums[i-1]) continue;
-                int start = i+1;
-                int end = nums.size()-1;
+                int start = i+1; int end = size - 1;
+        
                 while(start<end){
-                    if(start>=end) break;
                     if(nums[start]+nums[end] == target) {
                         res.push_back({nums[k], nums[i], nums[start], nums[end]}); 
                         start++; end--;
-                    }else if(nums[start]+nums[end]< target){
-                        start++;
-                    }else{
-                        end--;
-                    }
+                    }else if(nums[start]+nums[end]< target) start++;
+                    else end--;
+                    
                     while(start>i+1 && nums[start]==nums[start-1]) start++;
                     while(end<nums.size()-1 && nums[end]==nums[end+1]) end--;
                 }
