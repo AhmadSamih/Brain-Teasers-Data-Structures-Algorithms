7e45b323180d2a1e32cdd77bd01baf35c5adb877
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-04 00:44:53 -0500

    [18. 4Sum][Accepted]committed by Ahmad Samih

--- 18-4Sum/solution.cpp
+++ 18-4Sum/solution.cpp
@@ -11,21 +11,22 @@ public:
         sort(nums.begin(), nums.end());
         
         for(int k=0; k<size;k++){
+            while(k>0 && nums[k]==nums[k-1]) k++;
+            
             int t = _target - nums[k];
-            if(k>0 && nums[k]==nums[k-1]) continue;
-        
             for(int i=k+1; i<size; i++){
+                while(i>k+1 && nums[i]==nums[i-1]) i++;
+                
                 int target = t - nums[i];
-                if(i>k+1 && nums[i]==nums[i-1]) continue;
                 int start = i+1; int end = size - 1;
         
                 while(start<end){
-                    if(nums[start]+nums[end] == target) {
+                    if(nums[start]+nums[end]< target) start++;
+                    else if(nums[start]+nums[end] > target) end--;
+                    else{
                         res.push_back({nums[k], nums[i], nums[start], nums[end]}); 
                         start++; end--;
-                    }else if(nums[start]+nums[end]< target) start++;
-                    else end--;
-                    
+                    }
                     while(start>i+1 && nums[start]==nums[start-1]) start++;
                     while(end<nums.size()-1 && nums[end]==nums[end+1]) end--;
                 }
