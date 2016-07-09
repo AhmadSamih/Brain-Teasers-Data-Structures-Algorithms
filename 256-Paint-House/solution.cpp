37eab4c88be5a91d0cb5d98becd7f4ed6ef2162a
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-09 13:59:24 -0500

    [256. Paint House][Accepted]committed by Ahmad Samih

--- 256-Paint-House/solution.cpp
+++ 256-Paint-House/solution.cpp
@@ -3,19 +3,24 @@ public:
     int minCost(vector<vector<int>>& costs) {
     if(costs.empty() || costs[0].empty()) return 0;
     int n = costs.size();
-    vector<vector<int>>DP(n, vector<int>(costs[0].size(), INT_MAX));
+    vector<vector<int>>DP(n, vector<int>(2, INT_MAX));
     
     DP[0][0] = costs[0][0];
     DP[0][1] = costs[0][1];
     DP[0][2] = costs[0][2];
+    if(n==1) return min(min(DP[0][0], DP[0][1]), DP[0][2]);
     
     for(int i=1; i<n; i++){
-        DP[i][0] = costs[i][0] + min(DP[i-1][1],DP[i-1][2]);
-        DP[i][1] = costs[i][1] + min(DP[i-1][0],DP[i-1][2]);
-        DP[i][2] = costs[i][2] + min(DP[i-1][0],DP[i-1][1]);
+        DP[1][0] = costs[i][0] + min(DP[0][1],DP[0][2]);
+        DP[1][1] = costs[i][1] + min(DP[0][0],DP[0][2]);
+        DP[1][2] = costs[i][2] + min(DP[0][0],DP[0][1]);
+        
+        DP[0][0] =  DP[1][0];
+        DP[0][1] =  DP[1][1];
+        DP[0][2] =  DP[1][2];
     }
     
-    return min(min(DP[n-1][0], DP[n-1][1]), DP[n-1][2]);
+    return min(min(DP[1][0], DP[1][1]), DP[1][2]);
         
     }
 };
