7fc392b65f8dd1b3f5305396e4a9bd8a5b82afea
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-10 11:30:18 -0500

    [214. Shortest Palindrome][Accepted]committed by Ahmad Samih

--- 214-Shortest-Palindrome/solution.cpp
+++ 214-Shortest-Palindrome/solution.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     string shortestPalindrome(string s) {
         
-        string tmp = s;
+        string tmp, tmp2, tmp3;
+        
+        tmp = s;
         reverse(tmp.begin(), tmp.end());
         
-        if(tmp == s) {return s; }//nothing to add
+        if(tmp == s)
+            return s;//nothing to add
+            
         int idx = 1;
         int n =tmp.size();
-        string tmp2, tmp3;
         while(idx<n){
             
             tmp3 = s.substr(0, n -idx); 
             tmp2 = tmp.substr(idx);
-            if(tmp2 == tmp3) {return tmp.substr(0,idx)+s;}
+            if(tmp2 == tmp3) return tmp.substr(0,idx)+s;
             idx++;
         }
         return tmp;
