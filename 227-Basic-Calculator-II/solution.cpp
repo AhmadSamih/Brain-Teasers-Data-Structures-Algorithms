0d3488f1e1fac303c51b1c7e80f6ef5e19f6c67f
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-11 18:56:34 -0500

    [227. Basic Calculator II][Accepted]committed by Ahmad Samih

--- 227-Basic-Calculator-II/solution.cpp
+++ 227-Basic-Calculator-II/solution.cpp
@@ -2,46 +2,58 @@ class Solution {
 public:
     int calculate(string s) {
         
-        vector<int>num;
-        char * dup = strdup(s.c_str());
-        char * token = strtok(dup, " +-/*");
+        stack<int>stk;
+        
+        vector<int>nums;
+        vector<string>ops;
+        
+        //how about multi digit numbers ???????
+        
+        
+        char * tmp = strdup(s.c_str());
+        char * token = strtok(tmp, " +-/*");
         while(token){
-            num.push_back(stoi(token));
+            nums.push_back(stoi(token));
             token = strtok(NULL, " +-/*");
         }
-        free(dup);
+        free(tmp);
         free(token);
-
-        vector<char>ops;
-        ops.push_back('+');
-        for(int i=0; i<s.size();i++)
-            if(s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*')
-                ops.push_back(s[i]);
-            
-        stack<int>stk;    
-        for(int i=0; i<ops.size();i++){
-            if(ops[i] == '+')
-                stk.push(num[i]);
-            if(ops[i] == '-')
-                stk.push(-num[i]);
-            if(ops[i] == '*'){
-                int res = stk.top() * num[i];
-                stk.pop();
-                stk.push(res);
-            }
-            if(ops[i] == '/'){
-                int res = stk.top() / num[i];
-                stk.pop();
-                stk.push(res);
-            }
+        
+        
+        ops.push_back("+");
+        tmp = strdup(s.c_str());
+        token = strtok(tmp, " 0123456789");
+        while(token){
+            ops.push_back(token);
+            token = strtok(NULL, " 0123456789");
         }
-
-        int sum = 0;
-        while(!stk.empty()){
-            sum+=stk.top();
-            stk.pop();
+        free(tmp);
+        free(token);
+        
+        for(int i=0; i<ops.size();i++){
+            if(ops[i] == "+") {
+                stk.push(nums[i]);
+            }
+            if(ops[i] == "-") {
+                stk.push(-nums[i]);
+            }
+            if(ops[i] == "*") {
+                int tmp = stk.top();
+                stk.pop();
+                stk.push(tmp*nums[i]);
+            }
+            if(ops[i] == "/") {
+                int tmp = stk.top();
+                stk.pop();
+                stk.push(tmp/nums[i]);
+            }
         }
         
+        int sum = 0;
+        while(!stk.empty()){
+            sum+= stk.top();
+            stk.pop();        
+        }
         return sum;
     }
 };
