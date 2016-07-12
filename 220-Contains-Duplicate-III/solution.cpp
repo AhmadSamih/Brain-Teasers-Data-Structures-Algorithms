0eec7fabe24cdb2b3c2b235367ca990e5916a185
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-11 20:15:49 -0500

    [220. Contains Duplicate III][Accepted]committed by Ahmad Samih

--- 220-Contains-Duplicate-III/solution.cpp
+++ 220-Contains-Duplicate-III/solution.cpp
@@ -9,7 +9,7 @@ public:
             if(itr != dq.end() && abs(*itr-nums[i])<=t) return true;
 
             //push new one to deque
-            dq.insert(itr, nums[i]);
+            dq.insert(lower_bound(dq.begin(), dq.end(), nums[i]), nums[i]);
             
             //elimintate first if bigger than k
             if(dq.size()>k)
@@ -18,4 +18,4 @@ public:
         return false;
     }
 };
-       
+        
