4859044aec78e9d579e0974c5a6e64fe21aac433
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-11 20:15:31 -0500

    [220. Contains Duplicate III][Accepted]committed by Ahmad Samih

--- 220-Contains-Duplicate-III/solution.cpp
+++ 220-Contains-Duplicate-III/solution.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int _t) {
-        if(!nums.size() || !k) return false;
-        vector<long long>bst;
-        for(int i=0; i<nums.size(); i++)
-        {
-            //const auto it = lower_bound(bst.begin(), bst.end(), nums[i] - t);
-            //if(it != bst.end() && abs(*it - nums[i]) <= t) return true;
-            
-            const auto it = lower_bound(bst.begin(), bst.end(), nums[i]);
-            int idx = it - bst.begin();
-            long long num = nums[i];
-            long long t = _t;
-            if( idx>=0 && idx < bst.size() && abs(bst[idx]-  nums[i])<=t ) return true;
-            if( idx>0 && idx <= bst.size() && abs(bst[idx-1]-nums[i])<=t ) return true;
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        
+        vector<int>dq;
+        for(int i=0; i<nums.size(); i++){
+            //find if the diff of less than t exists
+            auto itr = lower_bound(dq.begin(), dq.end(), nums[i]-t);
+            if(itr != dq.end() && abs(*itr-nums[i])<=t) return true;
 
-            bst.insert(it, nums[i]);
-            if(bst.size() > k) bst.erase( find(bst.begin(), bst.end(), nums[i-k]) );
+            //push new one to deque
+            dq.insert(itr, nums[i]);
+            
+            //elimintate first if bigger than k
+            if(dq.size()>k)
+                dq.erase( find(dq.begin(), dq.end(), nums[i-k]) );
         }
         return false;
     }
 };
+       
