049896a259202ee0cfef1ca1b2c7c947bb8fc9c6
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-13 18:53:40 -0500

    [10. Regular Expression Matching][Accepted]committed by Ahmad Samih

--- 10-Regular-Expression-Matching/solution.cpp
+++ 10-Regular-Expression-Matching/solution.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
+
+    bool helper(string s, int si, string p, int pi){
+        
+        if(si==s.size() && pi==p.size()) return true;
+
+        if(p[pi+1] == '*')
+            return (si<s.size() && (s[si]==p[pi] || p[pi]=='.') && helper(s, si+1, p, pi)) || (helper(s, si, p, pi+2));
+        else
+            return (s[si]==p[pi] || p[pi]=='.') && helper(s, si+1, p, pi+1);
+    }
+
     bool isMatch(string s, string p) {
+        
+        return helper(s, 0, p, 0);
         
         vector<vector<bool>>DP(s.size()+1, vector<bool>(p.size()+1,0));
 
