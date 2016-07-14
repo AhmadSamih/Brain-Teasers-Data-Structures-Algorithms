70dee6e4fd2c0643c8dfb513edd490cbfb84fde0
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-14 01:07:24 -0500

    [44. Wildcard Matching][Accepted]committed by Ahmad Samih

--- 44-Wildcard-Matching/solution.cpp
+++ 44-Wildcard-Matching/solution.cpp
@@ -6,30 +6,30 @@ public:
        _ismatch[0][0] = true;
 
       //convert ****** => *
-      string pd = "";
-      for(int i =0; i<n; ){
+      string pd = p;
+      /*for(int i =0; i<n; ){
           pd += p[i];
           if(p[i] == '*'){
                 while(p[i] == '*') i++;
           }else{
             i++;
           }
-      }
+      }*/
 
-      n = pd.size();
-      if(pd[0] == '*') 
-        _ismatch[0][1] =  true;
+      for(int i=1; i<=n; i++)
+        if(p[i-1] == '*') 
+            _ismatch[0][i] =  _ismatch[0][i-1];
 
       for(int i =1; i<=m; i++){
           for(int j =1; j<=n; j++){
               if(pd[j-1] != '*'){
-                if(s[i-1] == pd[j-1] || pd[j-1] == '?') _ismatch[i][j] = _ismatch[i-1][j-1];
-                else _ismatch[i][j] = false;
+                _ismatch[i][j] = (s[i-1] == pd[j-1] || pd[j-1] == '?') && _ismatch[i-1][j-1];
               }else{
-                    _ismatch[i][j] = _ismatch[i-1][j] || _ismatch[i][j-1];
+                 _ismatch[i][j] = _ismatch[i-1][j] || _ismatch[i][j-1];
               }
           }
       }
+
       return _ismatch[m][n];
     }
 };
