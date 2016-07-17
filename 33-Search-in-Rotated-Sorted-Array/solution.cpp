67c1f07d2e85185b98e0186017247417781eac38
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-17 17:41:37 -0500

    [33. Search in Rotated Sorted Array][Accepted]committed by Ahmad Samih

---
+++ 33-Search-in-Rotated-Sorted-Array/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int l = 0, r = nums.size()-1;
+        while(l<=r){
+           // cout << "L " << l << " R " << r << endl; 
+            if(l==r) 
+                if(nums[l]!=target) 
+                    return -1;
+                else return l;
+            
+            int m = (l+r)/2;
+/*            if(nums[m]==target) return m;
+            if(nums[l]==target) return l;
+            if(nums[r]==target) return r;
+  */      
+        if (nums[m] >= nums[l]) {
+            if (target <= nums[m] && target >= nums[l]) r = m;
+            else l = m + 1;
+        } else {
+            if (target > nums[m] && target <= nums[r]) l = m + 1;
+            else r = m;
+        }
+        
+        }
+     return l;   
+    }
+};
