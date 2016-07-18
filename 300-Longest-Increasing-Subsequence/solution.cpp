9d7dc98ab334b6e967ace837e930e30a95cf60a8
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-18 08:10:07 -0500

    [300. Longest Increasing Subsequence][Accepted]committed by Ahmad Samih

--- 300-Longest-Increasing-Subsequence/solution.cpp
+++ 300-Longest-Increasing-Subsequence/solution.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-     
+        
         if(nums.empty())return 0;           
+        int Max = 1;
         vector<int>DP(nums.size(),1);
         for(int i=1; i<nums.size(); i++){
             for(int j=0; j<i; j++){
                 if(nums[i]>nums[j])
                     DP[i] = max(DP[i], DP[j]+1);
+                    if(DP[i]>Max) Max = DP[i];
             }
         }
-        return *max_element(DP.begin(), DP.end());
+        return Max;
     }
 };
 
