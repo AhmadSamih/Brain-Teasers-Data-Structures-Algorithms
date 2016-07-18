c2458ad8db98bc538a6e8eda7e03ae46da70400b
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-17 20:05:52 -0500

    [280. Wiggle Sort][Accepted]committed by Ahmad Samih

--- 280-Wiggle-Sort/solution.cpp
+++ 280-Wiggle-Sort/solution.cpp
@@ -3,7 +3,7 @@ public:
     void wiggleSort(vector<int>& nums) {
         if(nums.size()<2)return;
         for(int i=1; i<nums.size();i++){
-            if((i&0x1 && nums[i]<nums[i-1]) || (!(i&0x1) && nums[i]>nums[i-1]))
+            if(i%2 == nums[i]<nums[i-1])
                 swap(nums[i], nums[i-1]);
         }
     }
