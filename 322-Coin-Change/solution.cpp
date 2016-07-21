97058c19571d69036a87fc4a5c3d0f5ac7c0ed66
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-20 20:40:36 -0500

    [322. Coin Change][Accepted]committed by Ahmad Samih

--- 322-Coin-Change/solution.cpp
+++ 322-Coin-Change/solution.cpp
@@ -5,8 +5,8 @@ public:
         vector<int>DP(amount+1,INT_MAX);
         DP[0] = 0;
         for(int j=0; j<coins.size();j++){
-            for(int i=1; i<=amount; i++){
-                if(i<coins[j] || DP[i-coins[j]] == INT_MAX) continue;
+            for(int i=coins[j]; i<=amount; i++){
+                if(DP[i-coins[j]] == INT_MAX) continue;
                 DP[i] = min(DP[i - coins[j]] + 1, DP[i]);
             }
         }
