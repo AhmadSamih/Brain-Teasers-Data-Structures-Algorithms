7ac4dfd18a00854eace88b265aaf1a938a125b0d
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-21 21:17:36 -0500

    [151. Reverse Words in a String][Accepted]committed by Ahmad Samih

---
+++ 151-Reverse-Words-in-a-String/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void reverseWords(string &s) {
+      if(s.empty()){
+          s.clear();
+          return;
+      } 
+      
+      stack<string> stk;
+      std::size_t begin = 0;
+      std::size_t end = 0;
+      while(end != string::npos){
+        begin = s.find_first_not_of(' ',end);
+        if(begin == string::npos) break;
+        end = s.find_first_of(' ',begin+1);
+        stk.push(s.substr(begin,end-begin));
+      }    
+      s.clear();
+      while(!stk.empty()){
+        s.append(stk.top());
+        if(stk.size() >1) s.append(" ");
+        stk.pop();
+      }
+    }        
+};
