ba92e6c3caee5fd2dbdac8ac4a6007eb6236bca6
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-22 00:15:26 -0500

    [65. Valid Number][Accepted]committed by Ahmad Samih

--- 65-Valid-Number/solution.cpp
+++ 65-Valid-Number/solution.cpp
@@ -2,24 +2,17 @@ class Solution {
 public:
   
     bool isNumber(string s) {
-        bool isnum = false;
-        if(s.empty())return isnum;
+        if(s.empty())return false;
         if(s.size() == 1) return  isdigit(s[0]);
 
        int first = s.find_first_not_of(' ');
        int last = s.find_last_not_of(' ');
        s = s.substr(first, (last-first+1));
 
-       bool e = false;
-       bool dot = false;
-        
-        int i = 0;
-        if(s[i] == '+'  || s[i] == '-')
-            i++;
-            
-        for(;i<s.size(); i++){
-            if(isdigit(s[i])){
-                continue;
+       bool e = false; bool dot = false;
+       for(int i=0;i<s.size(); i++){
+            if(i==0 && (s[i]=='+' || s[i]=='-')){
+
             }else if(!e && i>=1 && i<s.size()-1 && s[i]=='e'){
                 e = true;
                 if(i == 1) 
@@ -27,7 +20,7 @@ public:
                         return false;
                 if(s[i+1]=='+' || s[i+1]=='-'){
                     i++;
-                    if(i+1 >= s.size()) 
+                    if(i+1>= s.size()) 
                         return false;
                 }
             }else if(!dot && !e && s[i] == '.'){
@@ -35,11 +28,9 @@ public:
                 if(!((i>=1 && isdigit(s[i-1])) || (i<s.size()-1 && isdigit(s[i+1])))){ //to take care of 3. case
                     return false;
                 }
-            }else{
+            }else if(!isdigit(s[i]))
                 return false;
-            }
         }
-  
         return true;
         //check if first char is +- and ignore
         
