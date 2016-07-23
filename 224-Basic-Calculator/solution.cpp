fd15d5761f3319d2c8279ca0b0f7097792164a86
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-23 11:25:35 -0500

    [224. Basic Calculator][Accepted]committed by Ahmad Samih

--- 224-Basic-Calculator/solution.cpp
+++ 224-Basic-Calculator/solution.cpp
@@ -1,49 +1,28 @@
 class Solution {
 public:
     int calculate(string s) {
-        
-        if(s.size() == 0) return NULL;
         int sign = 1;
         int sum = 0;
-        string last_num = "";
+        int num = 0;
         stack <int> stk;
-        
         for(int i=0; i<s.size();i++){
-            
             if(s[i] == '('){
-                stk.push(sum);
-                stk.push(sign);
-                sum = 0;
-                sign = 1;
-                continue;
-            }
-            if(s[i] == ')'){
-                sum = sum * stk.top();
-                stk.pop();
-                sum = sum + stk.top();
-                stk.pop();
-                continue;
-            }
-            
-            if(s[i]==' ' || s[i] == '+')
-                continue;
-                
-            if(s[i] == '-'){
+                stk.push(sum); sum =0;
+                stk.push(sign); sign =1;
+            }else  if(s[i] == ')'){
+                sum = sum * stk.top(); stk.pop();
+                sum = sum + stk.top(); stk.pop();
+            }else if(s[i] == '-'){
                 sign = -1;
-                continue;
-            }
-            
-            last_num += s[i];
-            
-            if((i == s.size()-1) || (i < (s.size()-1) && !isdigit(s[i+1]))){
-                sum = sum + (sign * stoi(last_num));
-                sign = 1;
-                last_num = "";
-            }
+            }else if(isdigit(s[i])){
+                num = num*10 + s[i]-'0';    
+                if((i == s.size()-1) || (i < (s.size()-1) && !isdigit(s[i+1]))){
+                    sum = sum + sign*num;
+                    sign = 1;
+                    num = 0;
+                }
+            }   
         }
-        
         return sum;
-        
     }
- 
 };
