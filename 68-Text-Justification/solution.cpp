e52dda62e373e70a9efc41cede711a096f2163fb
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-24 23:37:12 -0500

    [68. Text Justification][Accepted]committed by Ahmad Samih

--- 68-Text-Justification/solution.cpp
+++ 68-Text-Justification/solution.cpp
@@ -1,66 +1,51 @@
 class Solution {
 public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        //check 2d iterators again
-        
         vector<int>words_per_line;
         vector<int>chars_per_line;
         
         int wrds = 0;
         int chars = 0;
         int prev_idx =0;
-        for(int i=0; i<=words.size();i++){
-            int spaces = i-prev_idx;
-            if( i<words.size() &&(chars + words[i].size() + spaces) <= maxWidth){
+        for(int i=0; i<words.size();i++){
+            if((chars + words[i].size() + (i-prev_idx)) <= maxWidth){
                 wrds+=1;
                 chars+=words[i].size();
             }else{
                 words_per_line.push_back(wrds);
                 chars_per_line.push_back(chars);
-                prev_idx = i;
-                if(i<words.size()){
-                    wrds = 1;
-                    chars = words[i].size();
-                }
+                prev_idx = i; wrds = 1;
+                chars = words[i].size();
             }
         }
+        words_per_line.push_back(wrds);
+        chars_per_line.push_back(chars);
         
 
         vector<string>res;        
         int wrd_idx = 0;
+        string str = "";
+        
         for(int i=0; i<words_per_line.size();i++){//num lines
-            string str = "";
+            
             int spaces = maxWidth - chars_per_line[i];
-            int ending = 0; 
-            int extra, sp_per_wrd;
-    
-            if(i == words_per_line.size()-1) {
-                sp_per_wrd = 1;
-                ending = spaces - (words_per_line[i]-1);
-                extra=0;
-            }else{
-                if(words_per_line[i] ==1 ) sp_per_wrd = spaces;
-                else {sp_per_wrd = spaces/(words_per_line[i]-1);
-                    extra = spaces%(words_per_line[i]-1);
-                }
-            }
+            int sp_per_wrd = spaces/(words_per_line[i] == 1? 1 : words_per_line[i]-1);
+            int extra = spaces % (words_per_line[i] == 1? 1 : words_per_line[i]-1);
 
-            if(words_per_line[i]<=2  && (i!=words_per_line.size()-1)){
-                str+=words[wrd_idx++];
-                while(spaces) {str+=" "; spaces--;}
-                if(words_per_line[i]==2)str+=words[wrd_idx++];
-            }else{
-                for(int k=0; k<words_per_line[i];k++){
-                    str+=words[wrd_idx++];
-                    if(k == words_per_line[i]-1) continue;
-                    for(int kk=0; kk<sp_per_wrd;kk++)str+= " ";
-                    if(extra){ str+= " "; extra--;}
-                }
-                
-                while(ending){str+=" "; ending--;}
+            for(int k=0; k<words_per_line[i]-1;k++){
+                str.append(words[wrd_idx++]);
+                str.append(i<words_per_line.size()-1 ? sp_per_wrd : 1, ' ');
+                if(extra && i<words_per_line.size()-1)
+                    str.append(" "), extra--;
             }
+            str.append(words[wrd_idx++]);
+            if(words_per_line[i]==1)
+                str.append(sp_per_wrd, ' ');
+            else if(i == words_per_line.size()-1) 
+                str.append(spaces - (words_per_line[i]-1), ' ');
 
             res.push_back(str);
+            str.clear();
         }
 
         return res;
