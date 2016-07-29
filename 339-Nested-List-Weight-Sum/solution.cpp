e8af91f25aa602ad5091386a1837761b27cb532f
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-28 22:45:37 -0500

    [339. Nested List Weight Sum][Accepted]committed by Ahmad Samih

---
+++ 339-Nested-List-Weight-Sum/solution.cpp
@@ -0,0 +1,43 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    stack<std::vector<NestedInteger>::iterator> stk_b;
+    stack<std::vector<NestedInteger>::iterator> stk_e;
+    int depthSum(vector<NestedInteger>& nestedList) {
+        if(nestedList.empty()) return 0;
+        int sum = 0;
+        stk_b.push(nestedList.begin());
+        stk_e.push(nestedList.end());
+        while(!stk_b.empty()){
+            if(stk_b.top() == stk_e.top()){
+                stk_b.pop();
+                stk_e.pop();
+            }else{
+                auto itr = stk_b.top()++;
+                if(itr->isInteger()){
+                    sum+= itr->getInteger()*stk_b.size();
+                }else{
+                    stk_b.push(itr->getList().begin());
+                    stk_e.push(itr->getList().end());
+                }
+            }
+        }
+        return sum;
+    }
+};
