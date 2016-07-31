2f1e5928dd615afdd9c7eaf67bc3a25946a2c9f7
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-30 22:41:08 -0500

    [71. Simplify Path][Accepted]committed by Ahmad Samih

--- 71-Simplify-Path/solution.cpp
+++ 71-Simplify-Path/solution.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     string simplifyPath(string path) {
         
+        stack<string>stk;
         char * dup = strdup(path.c_str());
         char * token = strtok(dup, "/");
-        stack<string>stk;
+        
         while(token){
             string tmp = string(token);
-            if(tmp == ".."){
-                if(!stk.empty())
-                    stk.pop();
-            }else if(tmp != "" && tmp != ".")
-                stk.push(tmp);
+            if(tmp == ".." && !stk.empty()) stk.pop();
+            if(tmp != "" && tmp != "." && tmp != "..") stk.push(tmp);
             token = strtok(NULL, "/");
         }
-
-        string result;
-        result.clear();
         if(stk.empty()) return "/";
+        
+        string result;
         while(!stk.empty()){
             result.insert(0,"/"+stk.top());
             stk.pop();
