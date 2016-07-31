4a40869927bbb3c087a8aee8aa808a3eb298cd58
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-31 18:28:07 -0500

    [336. Palindrome Pairs][Accepted]committed by Ahmad Samih

--- 336-Palindrome-Pairs/solution.cpp
+++ 336-Palindrome-Pairs/solution.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     vector<vector<int>> palindromePairs(vector<string>& words) {
         
-        vector<vector<int>> res;
-        
+        vector<vector<int>> result;
         unordered_map<string, int>cache;
-        for(int i=0;i<words.size();i++){
-            cache[words[i]]=i;
-        }
-        
-        for(int i=0; i<words.size();i++){
-            for(int j=0; j<=words[i].size();j++){
-                string left =  words[i].substr(0,j);
-                string right = words[i].substr(j);
+        for(int i=0; i<words.size(); i++)
+            cache[words[i]] = i;
+            
+        for(int i=0; i<words.size(); i++){
+            
+            for(int k=0; k<=words[i].size(); k++){
+                
+                string left = words[i].substr(0, k);
+                string right = words[i].substr(k);
+                
                 string rleft = left; reverse(rleft.begin(), rleft.end());
                 string rright = right; reverse(rright.begin(), rright.end());
-                if(cache.find(rleft)!=cache.end() && cache[rleft]!=i && right==rright ){
-                    res.push_back({i,cache[rleft]});
+                
+                if(right == rright && cache.count(rleft) && cache[rleft]!=i){
+                    result.push_back({i, cache[rleft]});
                 }
-                if(j && cache.find(rright)!=cache.end() && cache[rright]!=i && left==rleft ){
-                    res.push_back({cache[rright],i});
+                if(k&& left == rleft && cache.count(rright) && cache[rright]!=i){
+                    result.push_back({cache[rright], i});
                 }
             }
         }
-        return res;
+        return result;
     }
 };
