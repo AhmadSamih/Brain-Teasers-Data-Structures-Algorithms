b3904fc1e01f8de4d90beee0a4b3a5933f763609
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-31 20:20:42 -0500

    [1. Two Sum][Accepted]committed by Ahmad Samih

--- 1-Two-Sum/solution.cpp
+++ 1-Two-Sum/solution.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
+
+    static bool cmp(pair<int,int>a, pair<int,int>b){
+        return a.first<b.first;
+    }
+        
     vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<pair<int,int>> remap(nums.size());
+        for(int i=0; i<nums.size(); i++)
+            remap[i] = {nums[i],i};
+        sort(remap.begin(), remap.end(), cmp);
+        
+        int start = 0;
+        int end = nums.size()-1;
+        while(start<end){
+            
+            if(remap[start].first + remap[end].first == target)
+                return {remap[start].second, remap[end].second};
+            
+            if(remap[start].first + remap[end].first < target)
+                start++;
+
+            if(remap[start].first + remap[end].first > target)
+                end--;
+        }
+        /*
         unordered_map<int, int>diff;
         vector<int>res;
         for(int i=0; i<nums.size();i++){
@@ -15,6 +40,7 @@ public:
                 return res;
             }
         }
-        return res;
+        */
+        return {0,0};
     }
 };
