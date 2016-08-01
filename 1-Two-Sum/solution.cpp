ee95bfb61f77107f4a9359261df642dda5e7f6c0
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-07-31 21:04:58 -0500

    [1. Two Sum][Accepted]committed by Ahmad Samih

--- 1-Two-Sum/solution.cpp
+++ 1-Two-Sum/solution.cpp
@@ -3,12 +3,9 @@ public:
 
     vector<int> twoSum(vector<int>& nums, int target) {
         //vector<vector<int>>remap(nums.size(), vector<int>(2));
-        int ** remap;
-        remap = new int * [nums.size()];
+        int ** remap = new int * [nums.size()];
         for(int i=0; i<nums.size();i++){
             remap[i] = new int [2];
-        }
-        for(int i=0; i<nums.size(); i++){
             remap[i][0] = nums[i]; remap[i][1] = i;
         }
         sort(remap, remap+nums.size(), [](int a[], int b[]){ return a[0] < b[0];});
@@ -16,13 +13,13 @@ public:
         int start = 0;
         int end = nums.size()-1;
         while(start<end){
-            if(remap[start][0] + remap[end][0] == target)
-                return {remap[start][1], remap[end][1]};
-            
             if(remap[start][0] + remap[end][0] < target)
                 start++;
-            else
+            else if(remap[start][0] + remap[end][0] > target)
                 end--;
+            else if(remap[start][0] + remap[end][0] == target)
+                return {remap[start][1], remap[end][1]};
+
         }
         /*
         unordered_map<int, int>diff;
