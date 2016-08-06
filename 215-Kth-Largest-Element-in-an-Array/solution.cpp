5416c38dd8b383fd4a638ec2484509dfd12705df
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-06 14:00:19 -0500

    [215. Kth Largest Element in an Array][Accepted]committed by Ahmad Samih

--- 215-Kth-Largest-Element-in-an-Array/solution.cpp
+++ 215-Kth-Largest-Element-in-an-Array/solution.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, std::less<int>>heap;
-        
-        for(int i=0; i<nums.size(); i++)
-            heap.push(nums[i]);
-        
+        priority_queue<int>heap(nums.begin(), nums.end());
         for(int i=0; i<k-1; i++)
             heap.pop();
             
