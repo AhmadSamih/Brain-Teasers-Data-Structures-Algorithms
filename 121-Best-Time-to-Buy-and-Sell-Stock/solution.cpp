f4f5688da8e869202001caa7332e39fa3027cc63
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-05 22:00:39 -0500

    [121. Best Time to Buy and Sell Stock][Accepted]committed by Ahmad Samih

--- 121-Best-Time-to-Buy-and-Sell-Stock/solution.cpp
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxCur = 0, maxSoFar = 0;
+        /*int maxCur = 0, maxSoFar = 0;
         for(int i = 1; i < prices.size(); i++) {
             maxCur = max(0, maxCur + prices[i] - prices[i-1]);
             maxSoFar = max(maxCur, maxSoFar);
         }
         return maxSoFar;
+        */
+        int size = prices.size();
+        vector<int>R(size,0);
+        for(int i=size-2; i>=0;i--){
+            R[i] = max(prices[i+1], R[i+1]);
+        }
+        int m = 0;
+        for(int i=0; i<size; i++){
+            m = max(m,R[i]-prices[i]);
+        }
+        return m;   
     }
 };
