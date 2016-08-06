fd8681a7e90e4781123fed0e0ddf2d3f565452af
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-06 13:21:31 -0500

    [69. Sqrt(x)][Accepted]committed by Ahmad Samih

--- 69-Sqrt(x)/solution.cpp
+++ 69-Sqrt(x)/solution.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long r = x;
-        while (r*r > x)
-            r = (r + x/r) / 2;
-        return r; 
+        if(x==1) return 1;
+        return bs(x, 1, x);
+    }
+    
+    int bs(int x, int left, int right){
+        while(left<right){
+            int mid = left + (right-left)/2;
+            if(mid>x/mid){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+        return left-1;
     }
 };
