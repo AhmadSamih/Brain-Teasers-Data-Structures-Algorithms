df4f8caac6d825eb0f09f53b9213a56903a75ae3
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-06 00:01:48 -0500

    [283. Move Zeroes][Accepted]committed by Ahmad Samih

--- 283-Move-Zeroes/solution.cpp
+++ 283-Move-Zeroes/solution.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int j = 0;
+        int tmp;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] !=0)
+            if(nums[i] !=0){
+                tmp = j;
                 nums[j++] = nums[i];
-                if(i!=(j-1)) nums[i] = 0;
+                i == tmp ? nums[i] = nums[i] : nums[i] = 0;
+            }
         }
     }
 };
