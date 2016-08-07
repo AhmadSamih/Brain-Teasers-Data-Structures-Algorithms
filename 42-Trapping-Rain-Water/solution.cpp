326a815aa54e8a851cf6013390ce02201c43bcab
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-06 19:33:54 -0500

    [42. Trapping Rain Water][Accepted]committed by Ahmad Samih

--- 42-Trapping-Rain-Water/solution.cpp
+++ 42-Trapping-Rain-Water/solution.cpp
@@ -4,13 +4,13 @@ public:
         if(height.empty()) return 0;
         vector<int>column(height.size(), 0);
         
-        for(int i=1 ;i<height.size(); i++){
-            while(i<height.size()-1 && height[i]<height[i-1])i++;
+        for(int i=1 ;i<height.size(); ++i){
+            while(i<height.size()-1 && height[i]<height[i-1])++i;
 
             int left_wall = 0;
             int right_wall = height[i];
             int left_wall_idx = 0;
-            for(int j=i-1; j>=0; j--){
+            for(int j=i-1; j>=0; --j){
                 if(left_wall>right_wall) break;
                 if(height[j]>left_wall){
                     left_wall = height[j];
@@ -18,13 +18,14 @@ public:
                 }
             }
             int prac = min (left_wall, right_wall);
-            for(int j=i-1; j>left_wall_idx; j--){ //walk between the two walls
+            for(int j=i-1; j>left_wall_idx; --j){ //walk between the two walls
                 column[j] = prac - height[j];
             }
         }
         
         int sum = 0;
-        for(auto x:column)sum+=x;
+        for(auto &x:column)
+            sum+=x;
 
         return sum;
     }
