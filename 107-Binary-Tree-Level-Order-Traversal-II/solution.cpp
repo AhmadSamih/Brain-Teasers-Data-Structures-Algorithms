6ec2bf26edb2ab3085cc525aed1711bd10a9edc5
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-07 14:22:32 -0500

    [107. Binary Tree Level Order Traversal II][Accepted]committed by Ahmad Samih

--- 107-Binary-Tree-Level-Order-Traversal-II/solution.cpp
+++ 107-Binary-Tree-Level-Order-Traversal-II/solution.cpp
@@ -15,31 +15,31 @@ public:
         vector<vector<int>>res; if(!root) return res;
         int depth = 1;
         q.emplace(root,1);
-        int new_level = 1;
+
         vector<int>lev;
         int last_level = 0;
         while(!q.empty()){
             TreeNode* tmp = q.front().first;
             int d = q.front().second;
-            if(d != last_level)
-                new_level = 1;
-            last_level = d;
-                
             q.pop();
+            
             if(tmp->left)
                 q.emplace(tmp->left, d+1);
+            
             if(tmp->right)
                 q.emplace(tmp->right, d+1);
-            if(new_level){
+            
+            if(d != last_level){
                 if(lev.size()) 
                     res.insert(res.begin(), lev);
-                new_level = 0;
                 lev.clear();
                 lev.push_back(tmp->val);
             }else{
                 lev.push_back(tmp->val);
             }
+            last_level = d;
         }
+        
         if(lev.size())
             res.insert(res.begin(), lev);
         return res;
