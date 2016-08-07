36116e57089b9973cb5ce48996d5f6f878761de7
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-07 15:12:29 -0500

    [226. Invert Binary Tree][Accepted]committed by Ahmad Samih

--- 226-Invert-Binary-Tree/solution.cpp
+++ 226-Invert-Binary-Tree/solution.cpp
@@ -11,21 +11,12 @@ class Solution {
 public:
     void mirror(TreeNode* root){
         if(!root) return;
-        
         TreeNode* tmp = root->left;
         root->left = root->right;
         root->right = tmp;
         mirror(root->left);
         mirror(root->right);
         
-    /*    if(!left || !right) return;
-        cout<<"hello\n";
-        int tmp = left->val;
-        left->val = right->val;
-        right->val = tmp;
-        mirror(left->left, right->right);
-        mirror(left->right, right->left);
-      */  
     }
     TreeNode* invertTree(TreeNode* root) {
         mirror(root);
