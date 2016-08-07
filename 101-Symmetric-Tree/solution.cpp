fb7a620f6c3fcf5e6f0c75274ca42d857ee89ecd
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-06 22:30:27 -0500

    [101. Symmetric Tree][Accepted]committed by Ahmad Samih

--- 101-Symmetric-Tree/solution.cpp
+++ 101-Symmetric-Tree/solution.cpp
@@ -17,10 +17,7 @@ public:
         bool left_right = (left->val == right->val) && rec(left->left, right->right);
         bool right_left = (left->val == right->val) && rec(left->right, right->left);
         
-        if(left_right && right_left)
-            return true;
-        else
-            return false;
+        return (left_right && right_left);
     }
 
     bool isSymmetric(TreeNode* root) {
