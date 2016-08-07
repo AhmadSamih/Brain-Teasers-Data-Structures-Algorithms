17960ce09fdee07c87fd6f55c1d97f9638939430
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-06 19:41:09 -0500

    [42. Trapping Rain Water][Accepted]committed by Ahmad Samih

--- 42-Trapping-Rain-Water/solution.cpp
+++ 42-Trapping-Rain-Water/solution.cpp
@@ -1,7 +1,32 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        if(height.empty()) return 0;
+        vector<pair<int,int>>R(height.size(), pair<int,int>(0,0));
+        vector<pair<int,int>>L(height.size(), pair<int,int>(0,0));
+        
+        for(int i=height.size()-2; i>=0; i--){
+            if(height[i+1] > R[i+1].first)
+                R[i].first = height[i+1], R[i].second = i+1;
+            else
+                R[i].first = R[i+1].first, R[i].second = R[i+1].second;    
+        }
+
+        for(int i=1; i<height.size(); i++){
+            if(height[i-1] > L[i-1].first)
+                L[i].first = height[i-1], L[i].second = i-1;
+            else
+                L[i].first = L[i-1].first, L[i].second = L[i-1].second;    
+        }
+        
+
+        int sum = 0;
+        for(int i=0; i<height.size();i++){
+            int m = min(R[i].first, L[i].first) - height[i];
+            if(m>0) sum +=m;
+        }
+        return sum;
+        
+ /*       if(height.empty()) return 0;
         vector<int>column(height.size(), 0);
         
         for(int i=1 ;i<height.size(); ++i){
@@ -28,5 +53,6 @@ public:
             sum+=x;
 
         return sum;
+ */
     }
 };
