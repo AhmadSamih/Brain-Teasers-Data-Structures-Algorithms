bd6ca66bafcae6a9a8093abab060022890d3740f
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-08 19:23:40 -0500

    [121. Best Time to Buy and Sell Stock][Accepted]committed by Ahmad Samih

--- 121-Best-Time-to-Buy-and-Sell-Stock/solution.cpp
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        //the difference between this and kadanes, is we start at 0 (no transaction could be done), in Kadane however, there has
+        //to be a minimum, which actually could be negative.
+        
         /*int maxCur = 0, maxSoFar = 0;
         for(int i = 1; i < prices.size(); i++) {
             maxCur = max(0, maxCur + prices[i] - prices[i-1]);
