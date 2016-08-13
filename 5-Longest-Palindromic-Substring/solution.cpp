df50e55bf4eae4fd5926d1d35d82e8a363078733
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-13 15:02:44 -0500

    [5. Longest Palindromic Substring][Accepted]committed by Ahmad Samih

--- 5-Longest-Palindromic-Substring/solution.cpp
+++ 5-Longest-Palindromic-Substring/solution.cpp
@@ -6,7 +6,6 @@ public:
         int final_start = 0;
         int final_end = 0;
 
-        //odd pal
         for(int i=0; i<size;i++){
             int start = i-1; int end = i+1;
             while(start>=0 && end<size && s[start]==s[end]){
@@ -15,11 +14,7 @@ public:
                 start--;
                 end++;
             }
-        }
-
-        //even pal
-        for(int i=0; i<size;i++){
-            int start = i-1; int end = i;
+            start = i-1; end = i;
             while(start>=0 && end<size && s[start]==s[end]){
                 if(final_end-final_start+1 < end-start+1)
                     final_start = start, final_end = end;
@@ -27,7 +22,7 @@ public:
                 end++;
             }
         }
-   
+
         return s.substr(final_start, final_end-final_start+1);
     }
 };
