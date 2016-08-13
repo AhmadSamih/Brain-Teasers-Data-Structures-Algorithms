425c30c2c2adccd0ce64037e4952de8029cac76a
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-13 11:27:59 -0500

    [347. Top K Frequent Elements][Accepted]committed by Ahmad Samih

--- 347-Top-K-Frequent-Elements/solution.cpp
+++ 347-Top-K-Frequent-Elements/solution.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
 
-    struct heapNode{
-      heapNode(int _value, int _freq):value(_value),freq(_freq){}
-      int value;
-      int freq;
-    };
-    
-    struct greatThan
-    {
-      bool operator()(const heapNode *l, const heapNode *r) const
-      {
-        return l->freq > r->freq;
+    struct greatThan{
+      bool operator()(pair<int,int>a, pair<int,int>b){
+        return a.second < b.second;
       }
     };
     
-    
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int> res;
-        priority_queue<heapNode*, vector<heapNode*>, greatThan> maxH;
         unordered_map<int,int>cache;
 
         for(auto &num:nums)
             cache[num]++;
+    
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greatThan> maxH(cache.begin(), cache.end());
 
-        for(auto itr = cache.begin(); itr!=cache.end(); itr++){
+        for(int i=0; i<k;i++){
+            res.push_back(maxH.top().first);
+            maxH.pop();
+        }
+/*        for(auto itr = cache.begin(); itr!=cache.end(); itr++){
             heapNode * x = new heapNode(itr->first, itr->second);
             maxH.push(x);
             if(maxH.size() > k){
@@ -38,6 +34,7 @@ public:
         res.insert(res.begin(), x->value);
         maxH.pop();
     }
-    return res;
+    */
+        return res;
     }
 };
