c4f5afa24c9dd901fec5d6432d2262e71be11cde
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-13 12:25:23 -0500

    [16. 3Sum Closest][Accepted]committed by Ahmad Samih

--- 16-3Sum-Closest/solution.cpp
+++ 16-3Sum-Closest/solution.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         int last_rem = 0;
-        int last_diff = INT_MAX;
         sort(nums.begin(), nums.end());
-        
+        int last_diff = INT_MAX;
         for(int i=0; i<nums.size(); i++){
             int rem = target - nums[i];
             int start = i+1; int end = nums.size()-1;
@@ -13,12 +12,8 @@ public:
                     last_diff = abs(rem - (nums[start]+nums[end]));                
                     last_rem = nums[i]+ nums[start] + nums[end];
                 }
-                
-                if(rem > (nums[start]+nums[end])){
-                    start++;
-                }else{
-                    end--;
-                }
+                if(rem > (nums[start]+nums[end])) start++;
+                else end--;
             }
         }
         return last_rem;
