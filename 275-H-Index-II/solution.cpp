56d43982efbbd38c0c67999feb98ae4e75171ef7
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-14 14:12:30 -0500

    [275. H-Index II][Accepted]committed by Ahmad Samih

--- 275-H-Index-II/solution.cpp
+++ 275-H-Index-II/solution.cpp
@@ -11,5 +11,19 @@ public:
                 return n-i;
         }
         return (n-i);
+        
+        //to optimize this, we can do it in a binary search approach
+        int left = 0;
+        int right = n-1;
+        while(left <=  right){
+            if(left == right) break;
+            int mid = (left+right)/2;
+            if(citations[mid]>= (n-mid)){
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }
+        return n-left;
     }
 };
