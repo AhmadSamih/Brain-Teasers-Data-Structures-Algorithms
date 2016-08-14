6718dcc2b84b3c2e0f38711c14101614ae257100
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-13 20:54:03 -0500

    [113. Path Sum II][Accepted]committed by Ahmad Samih

--- 113-Path-Sum-II/solution.cpp
+++ 113-Path-Sum-II/solution.cpp
@@ -9,25 +9,22 @@
  */
 class Solution {
 public:
-    void PathSum(TreeNode* root, int sum, vector<int> path, vector<vector<int>>& res) {
+    void PathSum(TreeNode* root, int sum, vector<int>& path, vector<vector<int>>& res) {
         if(!root) return;
         
-        if(root && root->left==NULL && root->right==NULL && sum-root->val == 0) {
-            path.push_back(root->val);
-            res.push_back(path);
-            return ;
-        }
-
         path.push_back(root->val);
+        if(!root->left && !root->right && sum==root->val)
+            res.push_back(path);
+
         PathSum(root->left, sum-root->val, path, res);
         PathSum(root->right, sum-root->val, path, res);
         path.pop_back();
-
     }
     
     vector<vector<int>> pathSum(TreeNode* root, int sum) {
         vector<vector<int>> res;
-        PathSum(root, sum, {}, res);
+        vector<int>path;
+        PathSum(root, sum, path, res);
         return res;
     }
 };
