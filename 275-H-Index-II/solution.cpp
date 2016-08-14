6d061ffa79e39a1568b498e1ff27ce23129540d8
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-14 14:26:32 -0500

    [275. H-Index II][Accepted]committed by Ahmad Samih

--- 275-H-Index-II/solution.cpp
+++ 275-H-Index-II/solution.cpp
@@ -3,14 +3,15 @@ public:
     int hIndex(vector<int>& citations) {
         //Explanation, max H-Index is number of papers
         int n = citations.size();
+        if(n == 0) return 0;
 
         //if the least cited paper has more than n citations, then H is n, otherwise, we try the next paper and compare it to n-1
-        int i = 0;
+        /*int i = 0;
         for(;i<n; i++){
             if(citations[i]>=(n-i))
                 return n-i;
         }
-        return (n-i);
+        return (n-i);*/
         
         //to optimize this, we can do it in a binary search approach
         int left = 0;
@@ -19,11 +20,11 @@ public:
             if(left == right) break;
             int mid = (left+right)/2;
             if(citations[mid]>= (n-mid)){
-                right = mid-1;
+                right = mid;//mid should not be decrement, because it might be mid that solves it
             }else{
                 left = mid+1;
             }
         }
-        return n-left;
+        return min(n-left, citations[left]);
     }
 };
