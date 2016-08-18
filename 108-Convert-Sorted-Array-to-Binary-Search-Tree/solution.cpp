ff95c7f557cb408f4ef554004c1626772154b3d1
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-17 20:36:36 -0500

    [108. Convert Sorted Array to Binary Search Tree][Accepted]committed by Ahmad Samih

--- 108-Convert-Sorted-Array-to-Binary-Search-Tree/solution.cpp
+++ 108-Convert-Sorted-Array-to-Binary-Search-Tree/solution.cpp
@@ -9,19 +9,19 @@
  */
 class Solution {
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        if(nums.empty())return NULL;
-        return DF(0, nums.size(), nums);
-    }
-    
-    TreeNode * DF(int left, int right, vector<int>&nums){
-        if(left>=right)return NULL;
-        int mid = (left+right)/2;
-        
-        TreeNode * tmp = new TreeNode(nums[mid]);
-        tmp->left = DF(left, mid, nums);
-        tmp->right = DF(mid+1, right, nums);
-        
-        return tmp;
-    }
+ TreeNode* dfs(vector<int>&nums, int left, int right){
+  if(left>right)
+    return NULL;
+  
+  int mid = (left+right)/2;
+  TreeNode* node = new TreeNode(nums[mid]);
+  node->left = dfs(nums, left,mid-1);
+  node->right = dfs(nums, mid+1,right);
+  
+  return node;
+}
+
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+  return dfs(nums, 0, nums.size()-1);
+}
 };
