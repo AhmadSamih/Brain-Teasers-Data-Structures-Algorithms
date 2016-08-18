f82b3dd83efb3d52a0e6f54fda6d1558b533b111
Ahmad A. Samih <ahmadsamih@gmail.com>
2016-08-17 21:42:59 -0500

    [10. Regular Expression Matching][Accepted]committed by Ahmad Samih

--- 10-Regular-Expression-Matching/solution.cpp
+++ 10-Regular-Expression-Matching/solution.cpp
@@ -12,6 +12,29 @@ public:
     }
 
     bool isMatch(string s, string p) {
-      return helper(s, 0, p, 0);
+        vector<vector<bool>>dp(s.size()+1, vector<bool>(p.size(), false));
+      
+        //i=0, j=0, empty strings
+        dp[0][0] = true;
+        
+        for(int i=1;i<=p.size();i++){
+            if(p[i-1] == '*')
+                dp[0][i] = (i>1 && dp[0][i-2]);
+        }
+        
+        //handle corner case of .* with empty string s
+        
+        for(int i=1; i<=s.size();i++){
+            for(int j=1;j<=p.size(); j++){
+                if(p[j-1]=='+'){
+                    
+                }else if(p[j-1] =='*'){
+                    dp[i][j] = (j>1 && dp[i][j-2]) || ((s[i-1]==p[j-2] || p[j-2]=='.') && dp[i-1][j]);
+                }else{
+                 dp[i][j] = (s[i-1]==p[j-1] || p[j-1]=='.') && dp[i-1][j-1];
+                }
+            }
+        }
+        return dp[s.size()][p.size()];
     }
 };
